ea1051756a4411b9d8e6d2edf2ddd3e54e4f9002
Kev_The_WebDev <kmgulledge@gmail.com>
2020-11-10 13:19:54 -0500

    update practice variables

---
+++ var.js
@@ -0,0 +1,50 @@
+// Var Practice 1
+
+var name = "pickles";
+
+console.log(name);
+
+// Instructions:
+// Create 3 variables. One called: "name", one called "pizzaCount", and one called "isHungry".
+// The name variable should be set to your name.
+// The pizzaCount variable should be set to a number.
+// The isHungry variable should be set to true or false.
+
+var name = "Kevin";
+var pizzaCount = 5;
+var isHungry = true;
+var totalCost = pizzaCount * 5.00;
+
+console.log("Welcome: " + name);
+console.log("Pizzas cost $5 each");
+console.log("Your total is: $" + totalCost);
+console.log("Still Hungry: " + isHungry);
+
+// Variables for Cars
+var carName = "Ford";
+var yearOfCar = "2020";
+
+console.log("Type of car you drive: " + carName);
+console.log("Year of your vehicle is " + yearOfCar);
+
+ // Notice that the confirm and prompt take in variables but the alert doesn't
+ var doYouRock = confirm("The question is, do *you* rock?");
+ var howMuchRock = prompt("How much do you rock?");
+
+ // Alert the results
+ alert(doYouRock);
+ alert(howMuchRock);;
+
+ // New Alert
+ var whatDoYouDrive = prompt("What type of vehicle do you drive?");
+ var whatYearOfVehicle = prompt("What is the year of the vehicle you drive?");
+ var doYouLikeYourVehicle = prompt("Do you like the vehicle you drive?");
+
+ alert(whatDoYouDrive);
+ console.log(whatDoYouDrive);
+ 
+ alert(whatYearOfVehicle);
+ console.log(whatYearOfVehicle);
+
+ alert(doYouLikeYourVehicle);
+ console.log(doYouLikeYourVehicle);
